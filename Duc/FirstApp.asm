253e35bfdc76f27f3c4595e7b59314b168243014
ducnguyen <hoangtayducnguyen@gmail.com>
2013-05-14 13:48:00 -0700

    Finir la squelette du main. Parser le command line et trouver les arguments

--- Duc/FirstApp.asm
+++ Duc/FirstApp.asm
@@ -9,23 +9,24 @@ comment * -----------------------------------------------------
         ----------------------------------------------------- *
 
     .data?
-      value dd ?
         CommandLine LPSTR ?
         pArrayCount DWORD ?
         pArrayOrigin LPSTR ?
-        tmpString LPSTR ?
+        arg1 byte 12 DUP(?)     ; ProcessWalk / DriveWalk
+        arg2 byte 100 DUP(?)    ; SHOULD CHANGE THIS CONSTANT 100
+        arg3 byte 100 DUP(?)    ; SHOULD CHANGE THIS CONSTANT 100
+        arg4 byte 7 DUP(?)      ; Import / Export
 
     .data
-        item dd 0
-        i BYTE 41h, 0
-        testArray dword 10 dup(53)
-        argv dword "ducd","abc", "abc", "abc"
-
+        processString byte  "ProcessWalk",0
+        driveString   byte  "DriveWalk",0
+        
     .code
 
 start:
    
 ; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+    call init
     call main
     print "Exit Program",13,10
     inkey
@@ -34,61 +35,84 @@ start:
 ; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 
 main proc
+    ; Compare string - Verify if we have to do Process Walk or Drive Walk
+    push    esi
+    push    edi
+    mov     esi,offset arg1
+    mov     edi,offset processString
+    mov     ecx,sizeof processString
+    repz    cmpsb
+    pop     edi
+    pop     esi
+    jz      processWalk
+    push    esi
+    push    edi
+    mov     esi,offset arg1
+    mov     edi,offset driveString
+    mov     ecx,sizeof driveString
+    repz    cmpsb
+    pop     edi
+    pop     esi
+    jz      driveWalk
+    print   "Your first argument is not correct (DriveWalk/ProcessWalk). Please verify the syntax", 13, 10
+    ret
+    
+    processWalk:
+        print "Put your processWalk code here", 13, 10
+        jmp finishWalk
+            
+    driveWalk: 
+        print   "Put your driveWalk code here", 13, 10
+
+    finishWalk:
+    
+    ; Process search and replace
+    print   "This is your search String:"
+    print   addr arg2,13,10
+    print   "This is your replace String:"
+    print   addr arg3,13,10
+
+
+    ; Process import/export section
+    print   "Import or export:"
+    print   addr arg4,13,10
+    ret
+main endp
+
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+; *************************************************************************
+;   Function name: init
+;   Description: Parse the commanline to get different arguments. 
+;                   Exit the program if user put invalid parameter
+; *************************************************************************
+init proc
 
     cls
-    print "Start Main function",13,10
 
-    invoke GetCommandLine  ;get CommandLine
+    invoke GetCommandLineW  ;get CommandLine
     mov CommandLine, eax
-    invoke StdOut, CommandLine
-
-    ; print a linebreak
-    mov eax, 0ah
-    push eax
-    invoke StdOut, esp
-    pop eax
     
     ;---------------------------------------------------------------------------;
 
     ; parse commandline - FAILED
     invoke CommandLineToArgvW, CommandLine, addr pArrayCount
     mov pArrayOrigin, eax
-    print str$(pArrayCount)
 
-    ; print a linebreak
-    mov eax, 0ah
-    push eax
-    invoke StdOut, esp
-    pop eax
+    ; Quit programm if user dont put 4 parameters
+    .IF pArrayCount != 5 
+        print "The programm need 4 parameters: DriveWalk/ProcessWalk APINameSearch APINameReplace Import/Export", 13, 10
+        ret
+    .ENDIF
 
-    ;---------------------------------------------------------------------------;
-
-    ; iterate through array
-    argc EQU LENGTHOF argv ; length of argument list
-    push ecx
-    mov ecx, 0
-        .while ecx < argc
-        push ecx
-        mov ecx, argv[ecx]
-        invoke dwtoa, ecx, addr tmpString
-        invoke StdOut, offset tmpString
-        print "end",13,10
-        pop ecx
-        add ecx, 1
-    .endw
-    pop ecx
-
-    ;mov ecx, testArray[0]
-    ;invoke dwtoa, ecx, addr tmpString
-    ;invoke StdOut, offset tmpString
-
-    
-
-    
+    Invoke  lstrcpy, addr arg1, cmd$(1)
+    Invoke  lstrcpy, addr arg2, cmd$(2)
+    Invoke  lstrcpy, addr arg3, cmd$(3)
+    Invoke  lstrcpy, addr arg4, cmd$(4)
 
     ret
-
-main endp
+init endp
 
 ; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 
