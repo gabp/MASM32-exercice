5d6fa34be11f3c041aadc146885cb3536323328d
Gab <plog2812@gmail.com>
2013-05-13 17:22:03 -0700

    failing build

--- Gab/console.asm
+++ Gab/console.asm
@@ -16,16 +16,17 @@ comment * -----------------------------------------------------
       hFile HANDLE ?
       ErrorCode DWORD ?
       array BYTE 1 DUP (?)
+      CurrentDirectory LPTSTR ?
       
     .data
       item dd 0
-      count BYTE 0
-      CurrentDirectory LPTSTR 200
-      TempDirectory BYTE " "
+      TempDirectory BYTE "\", 0
       FindFirstFileError BYTE "FindFirstFile() failed with code %d", 0
-      fileTypeAll BYTE "\*", 0
+      fileTypeAll BYTE "*", 0
       backslash BYTE "\", 0
       tmp BYTE " ", 0
+      null BYTE 00h, 0
+      
       
     .code
 
@@ -42,28 +43,35 @@ start:
 main proc
 
     cls
-    
+    invoke lstrcpy, addr array[0], addr backslash
+    invoke lstrcat, addr array[0], addr null
     invoke GetCurrentDirectory, 0, 00h
     invoke GetCurrentDirectory, eax, addr CurrentDirectory
     print addr CurrentDirectory, 13, 10
     print "-----------------------------", 13, 10
 
 FindDirectories:
-    ;invoke lstrcpy, addr temp, addr TempDirectory
     invoke lstrcpy, addr tmp, addr CurrentDirectory
+    invoke lstrcat, addr tmp, addr TempDirectory
     invoke lstrcat, addr tmp, addr fileTypeAll
-    invoke FindFirstFile, eax, addr fileData    
+
+    invoke FindFirstFile, addr tmp, addr fileData    
 
     mov hFile, eax
     mov ecx, 0
-    push ecx
+    push ecx ;number of elem in array
+    mov edx, 0
 
     .while ErrorCode != ERROR_NO_MORE_FILES
         .if fileData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY
             .if fileData.cFileName != 46
-                              
-                    invoke lstrcpy, addr tmp, addr backslash
+
+                    pop ecx
+                    push ecx
+                    invoke lstrcpy, addr tmp, addr array[edx]
+                    ;print addr tmp, 13, 10
                     invoke lstrcat, addr tmp, addr fileData.cFileName
+                    ;print addr tmp, 13, 10
                     pop ecx
                     push ecx
                     invoke lstrcpy, addr array[ecx], addr tmp
@@ -82,7 +90,9 @@ FindDirectories:
         invoke  GetLastError
         mov     ErrorCode, eax
     .endw
-
+print "-----------------------------", 13, 10
+    print addr array[0], 13, 10
+    print "-----------------------------", 13, 10
     pop ecx
     ret
 
