afe113f347163173cfe66127ee31d6cd6f611970
Gab <plog2812@gmail.com>
2013-05-16 11:21:46 -0700

    array example

---
+++ Gab/ArrayExample.asm
@@ -0,0 +1,74 @@
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+    include \masm32\include\masm32rt.inc
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+comment * -----------------------------------------------------
+                     Build this console app with
+                  "MAKEIT.BAT" on the PROJECT menu.
+        ----------------------------------------------------- *
+
+    .data?
+      value dd ?
+      array dword 500 dup(?)
+      
+
+    .data
+      item dd 0
+      string db "test", 0
+      string2 db "test2", 0
+    .code
+
+start:
+   
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+    call main
+    inkey
+    exit
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+main proc
+
+    cls
+
+    mov esi, offset array
+    mov eax, 0
+    push eax
+    .while eax < 10
+        mov eax, offset string
+        mov [esi], eax          
+
+        add esi, DWORD
+        
+        invoke lstrcpy, addr string, addr string2
+        mov eax, offset string
+        mov [esi], eax 
+
+        add esi, DWORD
+        pop eax
+        inc eax
+        push eax
+    .endw
+    pop eax
+
+    mov esi, offset array
+    mov eax, 0
+    push eax
+    .while eax < 20
+        print [esi], 13, 10        
+        
+        add esi, DWORD
+        pop eax
+        inc eax
+        push eax
+    .endw
+    pop eax
+    
+    ret
+
+main endp
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+end start
