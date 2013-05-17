deb0a37abb2af42bc0a5bc6bb643ab122850d40c
ducnguyen <hoangtayducnguyen@gmail.com>
2013-05-16 17:22:34 -0700

    Can store all file path in ProcessWalk

--- Duc/ProcessWalk/ProcessWalk.asm
+++ Duc/ProcessWalk/ProcessWalk.asm
@@ -13,13 +13,19 @@ comment * -----------------------------------------------------
         hProc dd ?
         ErrorCodeP DWORD ?
         bytesNeeded dd ?
-        fileName db 500 dup(?)
-        processName db 100 dup(?)
-        hMods dd 500 dup(?)
-
+        processName db ?
+        hMods dd 1000 dup(?)
+        fileList dword 500000 dup(?)
+        MAX_ARRAY_SIZE  dd  ?
 
     .data
-      processID DWORD 0
+        processID DWORD 0
+        countProcess DWORD 0
+        countModules  DWORD   0
+        position  DWORD   0
+        countTmp DWORD 0
+
+        MAX_STRING_LENGTH dd 200
 
     .code
 
@@ -36,10 +42,15 @@ start:
 main proc
 
     cls
+    ; Init some variables
+    mov     edi, 0
+    mov     eax, sizeof fileList
+    mov     MAX_ARRAY_SIZE, eax
 
 GetModules:
     invoke SetLastError, 0
-    
+
+    ; Get the process from processID
     mov eax, 0
     invoke OpenProcess, PROCESS_ALL_ACCESS, 0, processID
     mov hProc, eax
@@ -51,7 +62,15 @@ GetModules:
         ;print "Error opening process", 13, 10
         jmp NoHandle
     .endif
+
+    ; Increment the countProcess
+    push    eax
+    mov     eax, countProcess
+    inc     eax
+    mov     countProcess, eax
+    pop     eax
     
+    ; Get all modules of the selected process, store it in hMods array
     invoke EnumProcessModules, hProc, offset hMods, sizeof hMods, offset bytesNeeded
 
     invoke GetLastError
@@ -64,44 +83,77 @@ GetModules:
 
     mov esi, OFFSET hMods
 
-    invoke GetModuleBaseName, hProc, 0, addr processName, sizeof processName
-    print addr processName, 13, 10
-
     mov eax, 0
     push eax
-   innerloop:
-     cmp DWORD PTR [esi], NULL
-     jz interloop
-     invoke GetModuleFileNameEx, hProc, [esi], ADDR fileName, SIZEOF fileName
-     print "   "
-     print addr fileName, 13, 10
-     add esi, DWORD
-     pop eax
-     inc eax
-     push eax
+    
+innerloop:
+    .if     edi > MAX_ARRAY_SIZE
+        print   "OUT OF MEMORY - "
+        print   str$(edi), 13, 10
+        jmp     interloop
+    .endif
 
-    imul eax, 4
-     .if eax >= bytesNeeded
+    cmp     DWORD PTR [esi], NULL
+    jz      interloop
+    invoke  GetModuleFileNameEx, hProc, [esi], addr fileList[edi], 100
+
+    ;print   addr fileList[edi], 13, 10
+
+    add     esi, DWORD
+    pop     eax
+    inc     eax
+    push    eax
+    
+    imul    eax, 4
+    .if     eax >= bytesNeeded
         jmp interloop
-     .endif 
-     jmp innerloop
-   interloop:
+    .endif 
 
+    ;   Increment countModules
+    mov     eax,    countModules
+    inc     eax
+    mov     countModules, eax
+    ;   Increment edi counter
+    add     edi,    MAX_STRING_LENGTH
+    jmp     innerloop
+    
+interloop:
     pop eax
     
 NoHandle:
-    mov eax, processID
-    inc eax
-    mov processID, eax   
+    mov     eax,    processID
+    inc     eax
+    mov     processID, eax   
 
-    mov hProc, 0
-    mov hMods, 0
-    mov bytesNeeded, 0
+    mov     hProc,  0
+    mov     hMods,  0
+    mov     bytesNeeded, 0
+
+    .if     edi > MAX_ARRAY_SIZE
+        print "OUT OF MEMORY 2",13,10
+        jmp ShowResult
+    .endif
 
     .if processID < 100000
         jmp GetModules
     .endif
-    
+
+ShowResult: ; Testing purpose only, we can remove it in integration
+    mov     edi, 0
+    mov     ecx, 0
+    .WHILE  ecx < countModules
+        push    ecx
+        mov     eax,    ecx
+        print   str$(eax)
+        print   "-"
+        print   addr fileList[edi],13, 10
+        pop     ecx
+        
+        add     edi, MAX_STRING_LENGTH
+        inc     ecx
+    .ENDW
+
+    print   str$(countProcess), 13, 10
     ret
 
 main endp
