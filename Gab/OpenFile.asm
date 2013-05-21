2cca7b364a4c8a1e2d5dad5ac9a77c69c6247dff
Gab <plog2812>
2013-05-21 07:49:57 -0700

    how to open a file

---
+++ Gab/OpenFile.asm
@@ -0,0 +1,58 @@
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+    include \masm32\include\masm32rt.inc
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+comment * -----------------------------------------------------
+                     Build this console app with
+                  "MAKEIT.BAT" on the PROJECT menu.
+        ----------------------------------------------------- *
+
+    .data?
+      value dd ?
+      hFile dd ?
+      buffer dd 10000 dup(?)
+
+    .data
+      item dd 0
+      fileName db "console.exe", 0
+      bcnt dd 10000
+      var dd ?
+
+    .code
+
+start:
+   
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+    call main
+    inkey
+    exit
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+main proc
+
+    cls
+    
+    invoke CreateFile,addr fileName,GENERIC_READ,NULL,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL
+    mov hFile, eax
+
+    invoke GetLastError
+    print str$(eax), 13, 10
+
+    invoke ReadFile,hFile,addr buffer,bcnt,ADDR var,NULL
+    cmp		[var], 0
+	   je		readingDone
+readingDone:
+    invoke GetLastError
+    print str$(eax), 13, 10
+
+    print addr buffer, 13, 10
+
+    ret
+
+main endp
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+end start
