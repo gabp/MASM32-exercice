6a1ab4d095517ea45dc369482a89e74bf25ec777
Gab <plog2812@gmail.com>
2013-05-22 16:08:30 -0700

    import/export parsing

--- Gab/OpenFile.asm
+++ Gab/OpenFile.asm
@@ -14,14 +14,16 @@ comment * -----------------------------------------------------
       pMapping dd ?
       buffer dd 10000 dup(?)
       temp dd 512 dup(?)
+      numberOfNames dd ?
+      base dd ?
 
     .data
       item dd 0
-      fileName db "console.exe", 0
+      fileName db "ollydbg.exe", 0
       bcnt dd 10000
       var dd ?
-      NameTemplate db "%u %s",0 
-      OrdinalTemplate db "%u (ord.)",0 
+      NameTemplate db "  %s",0 
+      OrdinalTemplate db "  %u",0 
 
     .code
 
@@ -67,9 +69,104 @@ RVAToOffset PROC uses edi esi edx ecx pFileMap:DWORD,RVA:DWORD
    ret 
 RVAToOffset endp 
 
+ShowTheImports proc uses esi ecx ebx pNTHdr:DWORD
+    mov edi, pNTHdr
+    assume edi:ptr IMAGE_NT_HEADERS
+    mov edi, [edi].OptionalHeader.DataDirectory[sizeof IMAGE_DATA_DIRECTORY].VirtualAddress
+    invoke RVAToOffset, pMapping, edi
+    mov edi, eax
+    add edi, pMapping
+    assume edi:ptr IMAGE_IMPORT_DESCRIPTOR
+    .while !([edi].OriginalFirstThunk==0 && [edi].TimeDateStamp==0 && [edi].ForwarderChain==0 && [edi].Name1==0 && [edi].FirstThunk==0) 
+      invoke RVAToOffset, pMapping, [edi].Name1
+      mov edx,eax 
+      add edx,pMapping
+      pushad
+      print edx, 13, 10
+      popad
+ 
+      .if [edi].OriginalFirstThunk==0 
+         mov esi,[edi].FirstThunk 
+      .else 
+         mov esi,[edi].OriginalFirstThunk 
+      .endif
+ 
+      invoke RVAToOffset,pMapping,esi 
+      add eax,pMapping 
+      mov esi,eax
+ 
+      .while dword ptr [esi]!=0 
+         test dword ptr [esi],IMAGE_ORDINAL_FLAG32 
+         jnz ImportByOrdinal 
+         invoke RVAToOffset,pMapping,dword ptr [esi] 
+         mov edx,eax 
+         add edx,pMapping 
+         assume edx:ptr IMAGE_IMPORT_BY_NAME 
+         mov cx, [edx].Hint 
+         movzx ecx,cx 
+         invoke wsprintf,addr temp,addr NameTemplate,addr [edx].Name1 
+         jmp ShowTheText 
+  ImportByOrdinal: 
+         mov edx,dword ptr [esi] 
+         and edx,0FFFFh 
+         invoke wsprintf,addr temp,addr OrdinalTemplate,edx  
+  ShowTheText: 
+         pushad
+         print addr temp, 13, 10
+         popad
+         add esi,4 
+      .endw
+      add edi,sizeof IMAGE_IMPORT_DESCRIPTOR
+    .endw
+    ret
+ShowTheImports endp
+
+
+ShowTheExports proc uses esi ecx ebx pNTHdr:DWORD
+    mov edi, pNTHdr
+    assume edi:ptr IMAGE_NT_HEADERS
+    mov edi, [edi].OptionalHeader.DataDirectory.VirtualAddress   
+    .if edi==0 
+      print "No exports.", 13, 10 
+      ret 
+    .endif 
+    
+    invoke RVAToOffset, pMapping, edi
+    mov edi, eax
+    add edi, pMapping
+
+    assume edi:ptr IMAGE_EXPORT_DIRECTORY
+
+    invoke RVAToOffset, pMapping, [edi].nName
+    mov edx, eax
+    add edx, pMapping
+    print edx, 13, 10 ;print name of module
+
+    push [edi].NumberOfNames
+    pop numberOfNames
+
+    invoke RVAToOffset,pMapping,[edi].AddressOfNames 
+    mov esi,eax 
+    add esi, pMapping
+
+    .while numberOfNames > 0
+        invoke RVAToOffset,pMapping,dword ptr [esi] 
+        add eax, pMapping
+        
+        pushad
+          invoke wsprintf,addr temp,addr NameTemplate,eax
+          print addr temp, 13, 10
+        popad
+
+        dec numberOfNames
+        add esi, 4
+    .endw
+  ret
+ShowTheExports endp
+
+
+
 main proc
-        ;invoke GetLastError
-        ;print str$(eax), 13, 10
     cls
     
     invoke CreateFile,addr fileName,GENERIC_READ,NULL,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL ;ouverture du fichier
@@ -87,51 +184,11 @@ main proc
             add edi, [edi].e_lfanew 
             assume edi:ptr IMAGE_NT_HEADERS
             .if [edi].Signature==IMAGE_NT_SIGNATURE
-              mov edi, [edi].OptionalHeader.DataDirectory[sizeof IMAGE_DATA_DIRECTORY].VirtualAddress
-              invoke RVAToOffset, pMapping, edi
-              mov edi, eax
-              add edi, pMapping
-              assume edi:ptr IMAGE_IMPORT_DESCRIPTOR
-              invoke RVAToOffset, pMapping, [edi].Name1
-              mov edx,eax 
-              add edx,pMapping
-              pushad
-              print edx, 13, 10
-              popad
-
-              .if [edi].OriginalFirstThunk==0 
-                 mov esi,[edi].FirstThunk 
-              .else 
-                 mov esi,[edi].OriginalFirstThunk 
-              .endif
-
-              invoke RVAToOffset,pMapping,esi 
-              add eax,pMapping 
-              mov esi,eax
-
-              .while dword ptr [esi]!=0 
-                 test dword ptr [esi],IMAGE_ORDINAL_FLAG32 
-                 jnz ImportByOrdinal 
-                 invoke RVAToOffset,pMapping,dword ptr [esi] 
-                 mov edx,eax 
-                 add edx,pMapping 
-                 assume edx:ptr IMAGE_IMPORT_BY_NAME 
-                 mov cx, [edx].Hint 
-                 movzx ecx,cx 
-                 invoke wsprintf,addr temp,addr NameTemplate,ecx,addr [edx].Name1 
-                 jmp ShowTheText 
-          ImportByOrdinal: 
-                 mov edx,dword ptr [esi] 
-                 and edx,0FFFFh 
-                 invoke wsprintf,addr temp,addr OrdinalTemplate,edx  
-          ShowTheText: 
-                 pushad
-                 print addr temp, 13, 10
-                 popad
-                 add esi,4 
-              .endw
-
-              ;print "true", 13, 10
+                push edi
+                invoke ShowTheImports, edi
+                print "----------------------", 13, 10
+                pop edi
+                invoke ShowTheExports, edi
             .endif
           .endif
         .endif
